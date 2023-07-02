cac7d7a4c74c74acf9df64880ba97deb9e9fe56d
adiiii789 <69273351+adiiii789@users.noreply.github.com>
2023-07-02 03:37:44 +0200

    Objects work again

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -18,7 +18,7 @@ public class Player extends Entity {
 	
 	public final int screenX;
 	public final int screenY;
-//	public int hasKey = 0;
+	public int hasKey = 0;
 	
 	public Player (GamePanel gp, KeyHandler keyH) {
 		
@@ -146,39 +146,39 @@ public class Player extends Entity {
 		if(i != 999) {
 			
 			
-//			String objectName = gp.obj[i].name;
-//			
-//			switch (objectName) {
-//			
-//			case "Key":
-//				hasKey++;
-//				gp.obj[i] = null; //sobald der Schlüssel berührt wird, wird er "gelöscht"
-//				gp.ui.showMessage("You got a Key!");
-//				break;
-//			case "Door":
-//				if (hasKey > 0) {
-//					gp.obj[i] = null;
-//					hasKey--;
-//					gp.ui.showMessage("You opend the door!");
-//				}
-//				else {
-//					gp.ui.showMessage("You need a key!");
-//					
-//				}
-//				
-//				break;
-//			case "Boots":
-//				speed += 2; //wenn die Schuhe eingesammelt werden, wird der Spieler Schneller
-//				gp.obj[i] = null;
-//				gp.ui.showMessage("Speed up!");
-//				break;
-//			case "Chest":
-//				gp.ui.gameFinished = true;
-//				break;
-//				
-//			
-//			
-//			}
+			String objectName = gp.obj[i].name;
+			
+			switch (objectName) {
+			
+			case "Key":
+				hasKey++;
+				gp.obj[i] = null; //sobald der Schlüssel berührt wird, wird er "gelöscht"
+				gp.ui.showMessage("You got a Key!");
+				break;
+			case "Door":
+				if (hasKey > 0) {
+					gp.obj[i] = null;
+					hasKey--;
+					gp.ui.showMessage("You opend the door!");
+				}
+				else {
+					gp.ui.showMessage("You need a key!");
+					
+				}
+				
+				break;
+			case "Boots":
+				speed += 2; //wenn die Schuhe eingesammelt werden, wird der Spieler Schneller
+				gp.obj[i] = null;
+				gp.ui.showMessage("Speed up!");
+				break;
+			case "Chest":
+				gp.ui.gameFinished = true;
+				break;
+				
+			
+			
+			}
 			
 		}
 	}
